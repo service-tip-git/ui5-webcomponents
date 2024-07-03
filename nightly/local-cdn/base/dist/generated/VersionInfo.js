5362fe449f105632dd2ed5331fbbe1820d501c3e
service-tip-git <service-tip-git@users.noreply.github.com>
2024-07-03 01:17:27 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@84ba0aee47d0c1919d6b7206b8769accb6a03723 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.6",
     isNext: false,
-    buildTime: 1719882829,
+    buildTime: 1719969209,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/test/pages/ComponentStyles.html-279ba084.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-aab2dfc0.js";const n="_inputIcon_1wpmw_2";const o=document.getElementById("custom-icon");o.classList.add(n);

8a0ef73319573a01f17aa937d3d218914524a51f
service-tip-git <service-tip-git@users.noreply.github.com>
2024-08-24 01:19:31 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@5b29353c94f2794d61230e806ea593f8db83ab4a 🚀

--- nightly/assets/test/pages/Icon.html-81e9f181.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-cc1fd1eb.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.0",
     isNext: false,
-    buildTime: 1724375824,
+    buildTime: 1724462139,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map

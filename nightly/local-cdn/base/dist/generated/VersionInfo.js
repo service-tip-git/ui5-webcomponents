6701caba9715c58d7fe4bd309345f76127139f07
service-tip-git <service-tip-git@users.noreply.github.com>
2024-12-18 01:41:37 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@e122746d9f99f8f785b3f38e41db009a672cfa5d 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1734399629,
+    buildTime: 1734485855,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map

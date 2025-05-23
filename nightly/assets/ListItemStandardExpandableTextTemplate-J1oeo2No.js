6d3c5cb9ee2d8b587358dc20a1aec6c13c06cfb7
service-tip-git <service-tip-git@users.noreply.github.com>
2025-05-23 01:51:01 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@a70077fb580f677c486602d25bc136469491cbb5 🚀

---
+++ nightly/assets/ListItemStandardExpandableTextTemplate-J1oeo2No.js
@@ -0,0 +1,1 @@
+import{aa as x,as as m}from"./bundle.esm-Q0j0ZQk4.js";function p(a){const{className:t,text:e,maxCharacters:s,part:r}=a;return x(m,{part:r,class:t,text:e,maxCharacters:s})}export{p as default};
--- nightly/assets/test/pages/Icon.html-BODh1s7E.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-BWgd0aos.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();

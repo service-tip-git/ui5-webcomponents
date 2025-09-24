f06e2f25bbd6816eea93c2f7075e447d465c0068
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2025-09-24 14:51:46 +0300

    chore: cypress suites without env variables (#12359)

--- packages/main/cypress.config.js
+++ packages/main/cypress.config.js
@@ -1,21 +1,3 @@
 import cypressConfig from "@ui5/cypress-internal/cypress.config.js";
 
-const suites = {
-	SUITE1: [
-		"**/specs/base/*.cy.{jsx,tsx}",
-		"**/specs/[A-C]*.cy.{js,jsx,ts,tsx}",
-	],
-	SUITE2: [
-		"**/specs/[D-L]*.cy.{js,jsx,ts,tsx}",
-	],
-    SUITE3: [
-        "**/specs/[M-S]*.cy.{js,jsx,ts,tsx}",
-    ],
-    SUITE4: [
-        "**/specs/[T-Z]*.cy.{js,jsx,ts,tsx}",
-    ],
-};
-
-cypressConfig.component.specPattern = suites[process.env.TEST_SUITE] || ["**/specs/**/*.cy.{js,ts,jsx,tsx}"];
-
 export default cypressConfig;

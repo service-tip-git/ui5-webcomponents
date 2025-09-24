76f6f406a77d145dab24814371fdfb454cd613e7
ilhan orhan <ilhan.orhan007@gmail.com>
2025-09-24 11:34:42 +0300

    ci: run main cypress tests in 4 suites (#12355)
    * ci: run main cypress tests in 4 suites
    
    * chore: balance suites

--- packages/main/cypress.config.js
+++ packages/main/cypress.config.js
@@ -3,11 +3,17 @@ import cypressConfig from "@ui5/cypress-internal/cypress.config.js";
 const suites = {
 	SUITE1: [
 		"**/specs/base/*.cy.{jsx,tsx}",
-		"**/specs/[A-I]*.cy.{js,jsx,ts,tsx}",
+		"**/specs/[A-C]*.cy.{js,jsx,ts,tsx}",
 	],
 	SUITE2: [
-		"**/specs/[^A-I]*.cy.{js,jsx,ts,tsx}",
+		"**/specs/[D-L]*.cy.{js,jsx,ts,tsx}",
 	],
+    SUITE3: [
+        "**/specs/[M-S]*.cy.{js,jsx,ts,tsx}",
+    ],
+    SUITE4: [
+        "**/specs/[T-Z]*.cy.{js,jsx,ts,tsx}",
+    ],
 };
 
 cypressConfig.component.specPattern = suites[process.env.TEST_SUITE] || ["**/specs/**/*.cy.{js,ts,jsx,tsx}"];

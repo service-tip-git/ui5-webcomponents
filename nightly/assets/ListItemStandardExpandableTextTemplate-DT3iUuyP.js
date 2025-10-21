48ba1122fda5ab8fc5aadc7f205d40490be76ade
service-tip-git <service-tip-git@users.noreply.github.com>
2025-10-21 00:52:05 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@303247e6a16c802e123d97c9c27978a242008148 🚀

--- nightly/assets/ListItemStandardExpandableTextTemplate-DT3iUuyP.js
+++
@@ -1,1 +0,0 @@
-import{ae as x,av as m}from"./bundle.esm-DEJMmSBB.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};

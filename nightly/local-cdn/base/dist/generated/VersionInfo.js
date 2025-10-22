551d54e6bc52f29f7f62e5b27e2628168d4b77ac
service-tip-git <service-tip-git@users.noreply.github.com>
2025-10-22 00:53:14 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@303247e6a16c802e123d97c9c27978a242008148 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.0",
     isNext: false,
-    buildTime: 1761007670,
+    buildTime: 1761094135,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/fiori/dist/assets/ListItemStandardExpandableTextTemplate-AoaRBPT6.js
+++
@@ -1,1 +0,0 @@
-import{ae as x,av as m}from"./bundle.esm-C_vwK6QY.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};

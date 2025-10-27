a7e7bf3a0f78ed0635e4147a3edaf024c927eed4
GerganaKremenska <gergana.georgieva01@sap.com>
2025-10-27 15:30:02 +0200

    chore(ui5-carousel): allow all tests to run (#12551)

--- packages/main/cypress/specs/Carousel.cy.tsx
+++ packages/main/cypress/specs/Carousel.cy.tsx
@@ -389,7 +389,7 @@ describe("Carousel general interaction", () => {
 
 	});
 
-	it.only("navigateTo method and visibleItemsIndices", () => {
+	it("navigateTo method and visibleItemsIndices", () => {
 		cy.mount(
 			<Carousel id="carousel9" itemsPerPage="S2 M2 L2 XL2" arrowsPlacement="Navigation">
 				<Button>Button 1</Button>

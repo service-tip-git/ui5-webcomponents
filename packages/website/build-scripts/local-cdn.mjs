71ceeda4b6260d2c62a53076ffa41083db69f139
Nayden Naydenov <31909318+nnaydenow@users.noreply.github.com>
2025-12-05 10:51:13 +0200

    chore: fix website start on windows (#12772)

--- packages/website/build-scripts/local-cdn.mjs
+++ packages/website/build-scripts/local-cdn.mjs
@@ -33,5 +33,5 @@ const filesToDelete = files.filter(f => {
     return f.isFile() && !f.name.endsWith(".js")&& !f.name.endsWith(".json") && !f.name.endsWith(".svg") && !f.name.endsWith(".d.ts") && !f.name.endsWith("package.json")
 });
 
-console.log(filesToDelete.filter(f => f.path.includes("localization/dist/generated/assets/cldr")))
+// console.log(filesToDelete.filter(f => f.path.includes("localization/dist/generated/assets/cldr")))
 filesToDelete.map(f => rm(path.join(f.path ?? f.parentPath, f.name)));

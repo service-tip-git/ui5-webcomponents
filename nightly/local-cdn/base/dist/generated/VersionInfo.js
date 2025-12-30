82c90882e785dd3104cc1a9f70effc9a0f99bfe3
service-tip-git <service-tip-git@users.noreply.github.com>
2025-12-30 01:09:12 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents@405e213a695bef772c51cae1949672da3d9dce9d 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1766970598,
+    buildTime: 1767056672,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
--- nightly/local-cdn/main/dist/assets/test/pages/theming/Themes5.html-4mu5nU6R.js
+++
@@ -1,1 +0,0 @@
-import"../../../bundle.esm-BE9nk4-U.js";setTimeout(()=>{window["sap-ui-webcomponents-bundle"].configuration.setTheme("sap_horizon_hcb")},1e3);

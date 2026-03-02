30818c75627bb955f479fc4d675f64dbfce01806
Dobrin Dimchev <dobrin.dimchev@sap.com>
2026-03-02 17:35:57 +0200

    docs(ui5-shellbar-item): correct jsdocs annotations (#13174)

--- packages/fiori/src/ShellBarItem.ts
+++ packages/fiori/src/ShellBarItem.ts
@@ -25,7 +25,6 @@ type ShellBarItemAccessibilityAttributes = Pick<AccessibilityAttributes, "expand
  * @constructor
  * @extends UI5Element
  * @public
- * @experimental
  */
 @customElement({
 	tag: "ui5-shellbar-item",
@@ -58,6 +57,8 @@ class ShellBarItem extends UI5Element {
 
 	/**
 	 * Defines the item text.
+	 *
+	 * **Note:** The text is only displayed inside the overflow popover list view.
 	 * @default undefined
 	 * @public
 	 */
@@ -67,15 +68,31 @@ class ShellBarItem extends UI5Element {
 	/**
 	 * Defines the count displayed in badge.
 	 * @default undefined
+	 * @since 1.0.0-rc.6
 	 * @public
 	 */
 	@property()
 	count?: string;
 
 	/**
-	 * Defines accessibility attributes.
+	 * Defines additional accessibility attributes on Shellbar Items.
+	 *
+	 * The accessibility attributes support the following values:
+	 *
+	 * - **expanded**: Indicates whether the button, or another grouping element it controls,
+	 * is currently expanded or collapsed.
+	 * Accepts the following string values: `true` or `false`.
+	 *
+	 * - **hasPopup**: Indicates the availability and type of interactive popup element,
+	 * such as menu or dialog, that can be triggered by the button.
+	 *
+	 * - **controls**: Identifies the element (or elements) whose contents
+	 * or presence are controlled by the component.
+	 * Accepts a lowercase string value, referencing the ID of the element it controls.
+	 *
 	 * @default {}
 	 * @public
+	 * @since 2.9.0
 	 */
 	@property({ type: Object })
 	accessibilityAttributes: ShellBarItemAccessibilityAttributes = {};
